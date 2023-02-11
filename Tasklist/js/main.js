e5dd523de4d84eaba4623b30616a81dd9a1e303b
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-02-11 17:38:36 +0000

    Add files via upload

---
+++ Tasklist/js/main.js
@@ -0,0 +1,187 @@
+// Author: Kyan Keise
+// Date:
+// Title: 
+
+
+// Define UI Vars
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+// Load all event listeners
+
+loadEventListeners();
+
+// Load all event listeners
+
+function loadEventListeners() {
+    //DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks)
+    // Add task list
+    form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
+}
+
+// Get tasks from LS
+
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function (task) {
+        // Create li element 
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        // Create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append the link to li
+        li.appendChild(link);
+
+        // Append li to ul
+
+        taskList.appendChild(li);
+    });
+
+
+}
+
+// Add Task
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    // Create li element 
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+
+    taskList.appendChild(li);
+
+
+    // store in Local Storage
+    storeTaskInLocalStorage(taskInput.value);
+
+
+    // Clear input
+    taskInput.value = '';
+
+
+    e.preventDefault();
+
+}
+
+// Store Task
+
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task 
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you Sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+            // Remove from LS
+            removeTaskFromLocalStorage(
+                e.target.parentElement.parentElement);
+        }
+    }
+}
+
+// Remove from LS
+
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function (task, index) {
+        if (taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// clear Tasks
+function clearTasks() {
+    // slower version
+    // taskList.innerHTML = '';
+
+    // Faster version
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    // clear from LS
+    clearTasksFromLocalStorage();
+}
+
+// clear tasks from LS
+
+function clearTasksFromLocalStorage() {
+    localStorage.clear();
+}
+
+// filter tasks
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLocaleLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+
+        }
+    });
+}
---
+++ loan_calculator/HTML/js/main.js
@@ -0,0 +1,96 @@
+// Author: Kyan Keise
+// Date:
+// Title: 
+
+// Listen for submit
+
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    // hide results
+    document.getElementById('results').style.display = 'none';
+
+    // show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+
+// calculate results
+
+function calculateResults() {
+
+    console.log('calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principle = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatePayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+
+    const x = Math.pow(1 + calculatedInterest, calculatePayments);
+    const monthly = (principle * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatePayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatePayments) - principle).toFixed(2);
+
+        // show results
+        document.getElementById('results').style.display = 'block';
+
+        // hide loader
+        document.getElementById('loading').style.display = 'none';
+    } else {
+        showError('Please check your numbers')
+
+    }
+
+}
+
+// show error
+
+function showError(error) {
+
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    // hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // create div
+    const errorDiv = document.createElement('div');
+
+    // create elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add class
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // clear error after 3 seconds
+
+    setTimeout(clearError, 3000);
+
+
+}
+
+// clear error
+
+function clearError() {
+    document.querySelector('.alert').remove();
+}
